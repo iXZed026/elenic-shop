4b022077c0d9decc5ade51521ee1e52557db638b
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-11-16 16:28:49 +0330

    cimplete Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import Container from '../Container/Container'
+import NavLinks from './NavLinks/NavLinks'
+import Link from 'next/link'
+
+import { FaCartShopping } from "react-icons/fa6";
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className='bg-[#13141A] sticky top-0 border-b-[1px] border-b-[var(--green-color)]'>
+      <Container>
+        <div className='between-screen py-4'>
+          <div className="logo">
+            <h1 className='text-2xl text-[var(--green-color)] font-semibold'>Elenic Shop</h1>
+          </div>
+          <NavLinks />
+          <div className='center-screen gap-5'>
+            <Link href={"/cart"}><FaCartShopping className='icon'/></Link>
+            <button className='btn'>Sign Up</button>
+          </div>
+        </div>
+      </Container>
+    </header>
   )
 }
 
