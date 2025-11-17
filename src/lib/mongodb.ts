b55327e1cc3ca77480fb5cb2fb24f8a18e064303
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-11-17 13:07:24 +0330

    connect to the database

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI as string
+if (!uri) throw new Error("Check your conection string");
+
+let isConnected: boolean = false;
+
+export async function connectToDatabase() {
+    if (isConnected) console.log("Database is already connected.")
+    try {
+        await mongoose.connect(uri, { dbName: "elenicshop" })
+        isConnected = true;
+        console.log("is connected")
+    }
+    catch (err) {
+        console.log("An error occurred while connecting to the database. ", err)
+        // process.exit(1)
+    }
+
+}
