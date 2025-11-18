eabe5953c46d5ce51ccedc29c133ca3568ef9c91
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-11-18 15:49:15 +0330

    adding redux provider

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,24 @@
+"use client"
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 import { ChildrenProps } from '@/types/children'
 import Container from '../Container/Container'
+import { Provider } from 'react-redux'
+import { store } from '@/redux/store'
+
 
 function Layout({ children }: ChildrenProps) {
     return (
         <>
-            <Navbar />
-            <div className='page'>
-                <Container>
-                    {children}
-                </Container>
-            </div>
-            <Footer />
+            <Provider store={store}>
+                <Navbar />
+                <div className='page'>
+                    <Container>
+                        {children}
+                    </Container>
+                </div>
+                <Footer />
+            </Provider>
         </>
     )
 }
