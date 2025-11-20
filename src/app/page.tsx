5e322fd9d0c73d4b5c7b3c4551902990e34d3f4f
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-11-20 16:14:04 +0330

    changing hero description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ function Home() {
           <h1 className='text-7xl font-semibold text-[var(--green-color)]'>Welcome To Elenic Shop</h1>
         </div>
         <div className='mb-12'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aperiam velit accusamus, mollitia numquam in!</p>
+          <p>Discover the latest products with the best quality and price.
+            Shop smart, fast, and securely — your perfect online shopping experience starts here.</p>
         </div>
         <div className='center-screen gap-5 mb-12'>
           <button className='btn'>Go to Store</button>
