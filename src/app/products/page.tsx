6eb13c3ac3b410708cfa44cf708561d45a39a6a6
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-11-20 16:17:19 +0330

    create product redux structure

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+"use client"
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { getProductsServer } from '@/redux/store/Product';
+import { AppDispatch, RootState } from '@/redux/store';
 
 function Products() {
+
+
+  const dispatch = useDispatch<AppDispatch>();
+  let reducerProducts = useSelector((state: RootState) => state.products);
+
+  useEffect(() => {
+    dispatch(getProductsServer("http://localhost:3000/api/products"))
+  }, [])
+
+  console.log(reducerProducts)
+
   return (
     <div>Products</div>
   )
